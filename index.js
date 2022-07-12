cdcda0396a71469ece45b89809cb9c94e7785662
Min Khanthee <pkhanthee@gmail.com>
2022-07-12 12:12:25 +1200

    add api

---
+++ index.js
@@ -0,0 +1,8 @@
+document.getElementById('get-activity').addEventListener('click', () => {
+  fetch('http://www.boredapi.com/api/activity/')
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data)
+      document.getElementById('activity').textContent = data.activity
+    })
+})
